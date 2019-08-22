72b1f2df5aaf310d652c2b1da1a980f2ac1b0cde
Seungjae Moon <sjaymoon15@gmail.com>
2019-08-21 19:41:15 -0500

    Add a simple group route.

---
+++ routes/groupRoutes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+// const requireAuth = require('../middlewares/requireAuth');
+
+const Group = mongoose.model('Group');
+
+const router = express.Router();
+
+// router.use(requireAuth);
+
+// router.get('/groups', async (req, res) => {
+//   const groups = await Group.find({ userId: req.user._id });
+
+//   res.send(groups);
+// });
+
+router.get('/groups', async (req, res) => {
+  const groups = await Group.find({});
+
+  res.send(groups);
+});
+
+module.exports = router;
