b1ba27e8d4d36405dd4e35784d631b81e509c6c0
Seungjae Moon <sjaymoon15@gmail.com>
2019-08-27 17:27:52 -0500

    Update GET groups route to use requireAuth

--- routes/groupRoutes.js
+++ routes/groupRoutes.js
@@ -1,21 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
-// const requireAuth = require('../middlewares/requireAuth');
+const requireAuth = require('../middlewares/requireAuth');
 
 const Group = mongoose.model('Group');
 
 const router = express.Router();
 
-// router.use(requireAuth);
-
-// router.get('/groups', async (req, res) => {
-//   const groups = await Group.find({ userId: req.user._id });
-
-//   res.send(groups);
-// });
+router.use(requireAuth);
 
 router.get('/groups', async (req, res) => {
-  const groups = await Group.find({});
+  const groups = await Group.find({ userId: req.user._id });
 
   res.send(groups);
 });
